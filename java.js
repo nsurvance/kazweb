1b01909e79f89afd5e7ae46cda6ebd8dc3fbc4cc
genorpg <58082491+nsurvance@users.noreply.github.com>
2021-09-21 23:39:39 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -25,3 +25,45 @@ if (document.layers) {
 }
 
 document.oncontextmenu = new Function("return false")
+
+
+
+
+
+function preloader() {
+    if (document.images) {
+        var img1 = new Image();
+        var img2 = new Image();
+        var img3 = new Image();
+        var img4 = new Image();
+        var img5 = new Image();
+        var img6 = new Image();
+        var img7 = new Image();
+        var img8 = new Image();
+        var img9 = new Image();
+
+        img1.src = "images/thumbnails/1c.jpg";
+        img2.src = "images/thumbnails/3c.jpg";
+        img3.src = "images/thumbnails/4c.jpg";
+        img4.src = "images/thumbnails/5c.jpg";
+        img5.src = "images/thumbnails/6c.jpg";
+        img6.src = "images/thumbnails/7c.jpg";
+        img7.src = "images/thumbnails/8c.jpg";
+        img8.src = "images/thumbnails/9c.jpg";
+        img9.src = "images/thumbnails/10c.jpg";
+    }
+}
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+addLoadEvent(preloader);
